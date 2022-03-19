cc74e3988e1e7b82c3c9202fe5d13cf5467a8f4c
cheerfulleg <yolginivan@gmail.com>
2022-03-19 17:16:51 +0200

    added origin for react frontend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi_pagination import add_pagination
 from starlette.responses import HTMLResponse
@@ -25,7 +25,7 @@ async def index(request: Request):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
